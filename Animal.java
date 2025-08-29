3b0bf5dc666d1df95a919f44e9b97fe65e031443
revathy20060219 <revathy20060219@gmail.com>
2025-08-29 10:58:46 +0530

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,4 +1,4 @@
-class Animal {
+public class Animal {
     String name;
 
     public Animal(String name) {
@@ -11,41 +11,5 @@ class Animal {
 
     public void sleep() {
         System.out.println(name + " sleeps peacefully.");
-    }
-}
-
-class Cat extends Animal {
-    public Cat(String name) {
-        super(name);
-    }
-
-    public void meow() {
-        System.out.println(name + " meows softly.");
-    }
-}
-
-class Dog extends Animal {
-    public Dog(String name) {
-        super(name);
-    }
-
-    public void bark() {
-        System.out.println(name + " barks loudly.");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Dog dog = new Dog("Rocky");
-        dog.eat();
-        dog.sleep();
-        dog.bark();
-
-        System.out.println();
-
-        Cat cat = new Cat("Kitty");
-        cat.eat();
-        cat.sleep();
-        cat.meow();
     }
 }
